fc2996f5bb84e0fd874b0c4bada2ea306545bdc3
utkalikajena <utkalikajena97@gmail.com>
2021-05-07 12:07:24 +0530

    uc-2-sub

--- calculation.sh
+++ calculation.sh
@@ -8,7 +8,16 @@ function add() {
     local c=$(( $a + $b ))
     echo $c
 }
+function sub(){
+   local a=$1
+   local b=$2
+   local c=$(( $a - $b ))
+   echo $c
+}
 a=2
 b=4
 result="$(add $a $b)"
 echo $result
+
+resultSub="$(sub $a $b)"
+echo $resultSub
